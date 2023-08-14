a56ca7be4c32aeb700238af237ca9ea4bf1ccd5a
muhammadmehdi09 <120133578+muhammadmehdi09@users.noreply.github.com>
2023-08-14 22:07:18 +0500

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ export default function Home() {
           <h1 className='navbar-brand'>Muhammad Mehdi</h1>
         </div>
         <ul className='menu-items'>
-        <div className='logo'><img src='/logo.png' alt='Logo' className='w-16 mt-2 ml-2'/></div>
           <li><a href='#home'>Home</a></li>
           <li><a href='#about'>About</a></li>
           <li><a href='#contact-me'>Contact</a></li>
